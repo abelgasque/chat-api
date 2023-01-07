90a58a04f07739e8d170e470e615ab8cb9b30c96
Abel Gasque <abelgasque20@gmail.com>
2023-01-07 19:29:31 -0300

    Added package of Bootstrap

--- SecurityApp.Web/ClientApp/src/app/app.module.ts
+++ SecurityApp.Web/ClientApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { SharedModule } from './shared/shared.module';
 
     CoreModule,
     SharedModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
