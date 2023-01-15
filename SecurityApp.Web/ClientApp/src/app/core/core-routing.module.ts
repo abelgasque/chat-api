5f2b747e10160cae88512ef3c18aa6f7edded25c
Abel Gasque <abelgasque20@gmail.com>
2023-01-15 19:43:49 -0300

    feat: Created page redirection component.

--- SecurityApp.Web/ClientApp/src/app/core/core-routing.module.ts
+++ SecurityApp.Web/ClientApp/src/app/core/core-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageRedirectComponent } from '../shared/components/page-redirect/page-redirect.component';
 import { CoreComponent } from './core.component';
 
 const routes: Routes = [
@@ -10,6 +11,11 @@ const routes: Routes = [
       { path: '', loadChildren: () => import('src/app/modules/home/home.module').then(m => m.HomeModule) },
       { path: 'security', loadChildren: () => import('src/app/modules/security/security.module').then(m => m.SecurityModule) },
       { path: 'customer', loadChildren: () => import('src/app/modules/customer/customer.module').then(m => m.CustomerModule) },
+
+      { path: 'page-not-found', component: PageRedirectComponent },
+      { path: 'page-not-authorized', component: PageRedirectComponent },
+      { path: '', redirectTo: '', pathMatch: 'full' },
+      { path: '**', redirectTo: 'page-not-found' },
     ]
   }
 ];
