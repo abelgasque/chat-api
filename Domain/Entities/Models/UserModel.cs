262e1ab55e3de8dadb18e5c98d46fbb5c9cb6060
Abel Gasque <abelgasque20@gmail.com>
2025-07-18 14:32:09 -0300

    Update libs

--- Domain/Entities/Models/UserModel.cs
+++ Domain/Entities/Models/UserModel.cs
@@ -1,5 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
