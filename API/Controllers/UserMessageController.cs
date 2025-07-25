2febe6d790d6c5fcd8102aa86d92a99fc6d60e1f
Abel Gasque <abelgasque20@gmail.com>
2025-07-24 23:56:23 -0300

    Implement UserMessage filtering and enhance UserMessageController

--- API/Controllers/UserMessageController.cs
+++ API/Controllers/UserMessageController.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using ChatApi.API.Interfaces;
 using ChatApi.Domain.Entities.Tenants;
+using ChatApi.Infrastructure.Services;
+using ChatApi.Domain.Requests;
 
 namespace ChatApi.Controllers
 {
     [Route("v1/api/user/messages")]
     public class UserMessageController : BaseController<UserMessageModel, IBaseController<UserMessageModel>>
     {
-        public UserMessageController(IBaseController<UserMessageModel> service) : base(service) { }
+        protected new readonly UserMessageService _service;
+
+        public UserMessageController(
+            IBaseController<UserMessageModel> service,
+            UserMessageService userMessageService
+        ) : base(service)
+        {
+            _service = userMessageService;
+        }
+
+        /// <summary>
+        /// Endpoint that retrieves a list of entities based on filters
+        /// </summary>
+        /// <param name="filter">Filter parameters</param>
+        /// <returns>List of entities</returns>
+        /// <response code="200">Returns the list of entities</response>
+        [HttpGet]
+        public virtual async Task<IActionResult> Read([FromQuery] UserMessageFilterRequest filter)
+        {
+            var result = await _service.Read(filter);
+            return Ok(result);
+        }
     }
 }
