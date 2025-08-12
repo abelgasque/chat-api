c6bd19db57adbd59c60aa8467cf5bc4306b2f212
Abel Gasque <abel.lacerda@hypeone.com>
2025-08-12 09:42:18 -0300

    Update

--- Domain/Entities/Tenants/ChatMessageModel.cs
+++ Domain/Entities/Tenants/ChatMessageModel.cs
@@ -5,9 +5,12 @@ using ChatApi.Domain.Entities.Models;
 namespace ChatApi.Domain.Entities.Tenants
 {
     [Table("ChatMessages")]
-    public class ChatMessageModel : BaseModel
+    public class ChatMessageModel
     {
+        public Guid Id { get; set; }
         public Guid ChatId { get; set; }
+        public Guid SenderId { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
         public string Message { get; set; }
     }
 }
