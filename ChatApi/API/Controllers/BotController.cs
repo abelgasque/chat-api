070d8d7d08b71399885df94857f7b0d157fc69b5
Abel Gasque <55210807+abelgasque@users.noreply.github.com>
2025-08-29 17:42:45 -0300

    Update

--- ChatApi/API/Controllers/BotController.cs
+++ ChatApi/API/Controllers/BotController.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using ChatApi.API.Interfaces;
 using ChatApi.Domain.Entities.Tenants;
+using System.Threading.Tasks;
+using ChatApi.Domain.Requests;
+using ChatApi.Infrastructure.Services;
 
 namespace ChatApi.Controllers
 {
     [Route("v1/api/bot")]
     public class BotController : BaseController<BotModel, IBaseController<BotModel>>
     {
-        public BotController(IBaseController<BotModel> service) : base(service) { }
+        protected new readonly BotService _service;
+
+        public BotController(
+            IBaseController<BotModel> service,
+            BotService botService
+        ) : base(service)
+        {
+            _service = botService;
+        }
+
+        /// <summary>
+        /// Endpoint that retrieves a list of entities based on filters
+        /// </summary>
+        /// <param name="filter">Filter parameters</param>
+        /// <returns>List of entities</returns>
+        /// <response code="200">Returns the list of entities</response>
+        [HttpGet]
+        public virtual async Task<IActionResult> Read([FromQuery] PaginationRequest filter)
+        {
+            var result = await _service.Read(filter);
+            return Ok(result);
+        }
     }
 }
--- ChatApi/API/Controllers/TenantController.cs
+++ ChatApi/API/Controllers/TenantController.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using ChatApi.API.Interfaces;
 using ChatApi.Domain.Entities.Models;
+using ChatApi.Domain.Requests;
+using ChatApi.Infrastructure.Services;
 
 namespace ChatApi.Controllers
 {
     [Route("v1/api/tenant")]
     public class TenantController : BaseController<TenantModel, IBaseController<TenantModel>>
     {
-        public TenantController(IBaseController<TenantModel> service) : base(service) { }
+        protected new readonly TenantService _service;
+
+        public TenantController(
+            IBaseController<TenantModel> service,
+            TenantService tenantService
+        ) : base(service)
+        {
+            _service = tenantService;
+        }
+
+        /// <summary>
+        /// Endpoint that retrieves a list of entities based on filters
+        /// </summary>
+        /// <param name="filter">Filter parameters</param>
+        /// <returns>List of entities</returns>
+        /// <response code="200">Returns the list of entities</response>
+        [HttpGet]
+        public virtual async Task<IActionResult> Read([FromQuery] PaginationRequest filter)
+        {
+            var result = await _service.Read(filter);
+            return Ok(result);
+        }
     }
 }
